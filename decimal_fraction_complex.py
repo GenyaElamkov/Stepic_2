7cc25a61dd6b08ea989a5265114f100c62b3c0d6
GenyaElamkov <GenyaElamkov@gmail.com>
2022-11-21 22:24:17 +0300

    [upd] модуль fractions -> решены задачи с 1 - 11 шага

--- decimal_fraction_complex.py
+++ decimal_fraction_complex.py
@@ -1,5 +1,9 @@
 from decimal import Decimal
+from fractions import Fraction
+from math import factorial
 
+
+# Модуль Decimal.
 
 def decimal_1():
     """Выводит сумму наибольшего и наименьшего Decimal числа."""
@@ -46,8 +50,85 @@ def mathematical_expression():
     print(result)
 
 
+# Модуль Fraction
+
+
+def fraction_1():
+    """Каждого десятичного числа вывел его представление в виде обыкновенной дроби в формате."""
+    numbers = ['6.34', '4.08', '3.04', '7.49', '4.45', '5.39', '7.82', '2.76', '0.71', '1.97', '2.54', '3.67', '0.14',
+               '4.29', '1.84', '4.07', '7.26', '9.37', '8.11', '4.30', '7.16', '2.46', '1.27', '0.29', '5.12', '4.02',
+               '6.95', '1.62', '2.26', '0.45', '6.91', '7.39', '0.52', '1.88', '8.38', '0.75', '0.32', '4.81', '3.31',
+               '4.63', '7.84', '2.25', '1.10', '3.35', '2.05', '7.87', '2.40', '1.20', '2.58', '2.46']
+
+    for n in numbers:
+        print(f'{n} = {Fraction(n)}')
+
+
+def fraction_2():
+    """Выводит сумму наибольшего и наименьшего числа в виде обыкновенной дроби."""
+
+    s = '0.78 4.3 9.6 3.88 7.08 5.88 0.23 4.65 2.79 0.90 4.23 2.15 3.24 8.57 0.10 8.57 1.49 5.64 3.63 8.36 1.56 ' \
+        '6.67 1.46 5.26 4.83 7.13 1.22 1.02 7.82 9.97 5.40 9.79 9.82 2.78 2.96 0.07 1.72 7.24 7.84 9.23 1.71 6.24 ' \
+        '5.78 5.37 0.03 9.60 8.86 2.73 5.83 6.50 0.123 0.00021'
+
+    s = [Fraction(n) for n in s.split()]
+    print(sum([max(s), min(s)]))
+
+
+def shorten_fraction():
+    """Сокращает дробь и выводит ее"""
+    n, d = [int(input()) for _ in range(2)]
+
+    print(Fraction(n, d))
+
+
+def operations_fractions():
+    """Вычисляет и выводит их сумму, разность, произведение и частное."""
+    n1, n2 = [input() for _ in range(2)]
+    print(f'{n1} + {n2} = {Fraction(n1) + Fraction(n2)}')
+    print(f'{n1} - {n2} = {Fraction(n1) - Fraction(n2)}')
+    print(f'{n1} * {n2} = {Fraction(n1) * Fraction(n2)}')
+    print(f'{n1} / {n2} = {Fraction(n1) / Fraction(n2)}')
+
+    """
+    operators = {'+': F.__add__, '-': F.__sub__, '*': F.__mul__, '/': F.__truediv__}
+    a, b = [input() for _ in '..']
+    ops = [*map(F, (a, b))]
+    for op in operators:
+        print(f'{a} {op} {b} = {operators[op](*ops)}')
+    """
+
+
+def sum_fractions_1():
+    """Вычисляет и выводит рациональное число, равное значению выражения."""
+
+    n = int(input())
+
+    res = sum([Fraction(1, i) ** 2 for i in range(1, n + 1)])
+    print(res)
+
+
+def sum_fractions_2():
+    """Вычисляет и выводит рациональное число, равное значению выражения."""
+    n = int(input())
+    res = sum([Fraction(1, factorial(i)) for i in range(1, n + 1)])
+    print(res)
+
+
+def young_mathematician():
+    """Находит наибольшую правильную несократимую дробь с суммой числителя и знаменателя равной n."""
+    pass
+
+
 def main():
-    mathematical_expression()
+    young_mathematician()
+    # sum_fractions_2()
+    # sum_fractions_1()
+    # operations_fractions()
+    # shorten_fraction()
+    # fraction_2()
+    # fraction_1()
+    # mathematical_expression()
     # decimal_3()
     # decimal_2()
     # decimal_1()
