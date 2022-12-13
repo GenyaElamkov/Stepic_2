640c6c6cfdf4cffe942e275b44f71a2f10119f67
GenyaElamkov <GenyaElamkov@gmail.com>
2022-12-13 17:16:13 +0300

    [upd] модуль functions->часть 2, 11 шаг

--- functions.py
+++ functions.py
@@ -9,12 +9,38 @@ def matrix(n=1, m=None, value=0):
         matrix(n, m, value) — возвращает матрицу из n строк и m столбцов, в которой каждый
         элемент равен числу value.
     """
-    if not m: m = n
+    if not m:
+        m = n
     return [[value for _ in range(m)] for _ in range(n)]
 
 
+def count_args(*args):
+    """Принимает произвольное количество аргументов и возвращает количество переданных в нее аргументов."""
+    return len(args)
+
+
+def sq_sum(*args):
+    """Принимает произвольное количество числовых аргументов и возвращает сумму их квадратов."""
+    return sum(n ** 2 for n in args)
+
+
+def mean(*args):
+    """
+    Принимает произвольное количество аргументов и возвращает среднее арифметическое
+    переданных в нее числовых (int или float) аргументов.
+    """
+
+    arr = [arg for arg in args if type(arg) == int or type(arg) == float]
+    if not arr:
+        return 0.0
+    return sum(arr) / len(arr)
+
+
 def main():
-    print(matrix(3), sep='\n')
+    print(mean(-1, 2, 3, 10, ('5')))
+    # print(sq_sum(2))
+    # print(count_args([], (''), 'a', 12, False))
+    # print(matrix(3), sep='\n')
 
 
 if __name__ == '__main__':
