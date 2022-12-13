3b1233eb5514035cfbca09d3218e577365237da9
GenyaElamkov <GenyaElamkov@gmail.com>
2022-12-13 18:48:12 +0300

    [upd] модуль functions->часть 2 закрыт

--- functions.py
+++ functions.py
@@ -36,8 +36,54 @@ def mean(*args):
     return sum(arr) / len(arr)
 
 
+def greet(name, *args):
+    """
+    Принимает произвольное количество аргументов строк имен
+    (как минимум одно) и возвращает приветствие в соответствии с образцом.
+    """
+    text = f'Hello, {name}'
+    if not args:
+        text += '!'
+    else:
+        text += ' and ' + ' and '.join(args) + '!'
+    return text
+    # return f'Hello, {" and ".join((name,) + args)}!'
+
+
+def print_products(*args):
+    """
+    Принимает произвольное количество аргументов и выводит список продуктов
+    (любая непустая строка) по образцу: <номер продукта>) <название продукта>
+    (нумерация продуктов начинается с единицы). Если среди переданных аргументов нет ни одного продукта,
+    необходимо вывести текст Нет продуктов.
+    """
+    counter = 0
+    for product in args:
+        if type(product) == str and product:
+            counter += 1
+            print(f'{counter}) {product}')
+
+    if counter == 0:
+        print('Нет продуктов')
+
+
+def info_kwargs(**kwargs):
+    """
+    Принимает произвольное количество именованных аргументов и печатает именованные
+    аргументы в соответствии с образцом: <имя аргумента>: <значение аргумента>, при этом
+    имена аргументов следуют в алфавитном порядке (по возрастанию).
+    """
+    for k, v in sorted(kwargs.items()):
+        print(f'{k}: {v}')
+
+    # return {k: v for k, v in sorted(kwargs.items())}
+
+
 def main():
-    print(mean(-1, 2, 3, 10, ('5')))
+    print(info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher'))
+    # print_products('Бананы', [1, 2], ('Stepik',), 'Яблоки', '', 'Макароны', 5, True)
+    # print(greet('Timur'))
+    # print(mean(-1, 2, 3, 10, ('5')))
     # print(sq_sum(2))
     # print(count_args([], (''), 'a', 12, False))
     # print(matrix(3), sep='\n')
