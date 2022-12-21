c9dc1fba622f566e66ca2489a8e80757a3328a04
GenyaElamkov <GenyaElamkov@gmail.com>
2022-12-21 16:29:07 +0300

    [upd] модуль functions-> модуль 5 в работе

--- functions.py
+++ functions.py
@@ -232,6 +232,21 @@ def interesting_sorting_2():
     print(*sorted(numbers, key=comparator))
     """
 
+def map(function, items):
+    result = []
+    for item in items:
+        result.append(function(item))
+    return result
+
+def rounds_2():
+    """
+    Программу, которая с помощью функции map() округляет все элементы списка numbers до 22 десятичных знаков,
+    а затем выводит их, каждый на отдельной строке.
+    """
+    numbers = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.12013, 23.22222, 90.09873,
+               45.45, 314.1528, 2.71828, 1.41546]
+    round()
+
 
 def main():
     interesting_sorting_2()
