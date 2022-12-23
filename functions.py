f2582314bce7bf137fc2f0b9bb483d152393e537
GenyaElamkov <GenyaElamkov@gmail.com>
2022-12-23 17:00:53 +0300

    [upd] модуль functions-> модуль 5 закрыт

--- functions.py
+++ functions.py
@@ -261,11 +261,14 @@ def filter(function, items):
             result.append(item)
     return result
 
+
 def select_num(num):
     return num % 5 == 2 and len(str(num)) == 3
 
+
 def get_cube(num):
-    return num**3
+    return num ** 3
+
 
 def selects_three_digit_numbers():
     """
@@ -280,10 +283,73 @@ def selects_three_digit_numbers():
                120, 340, 963, 832, 1127]
 
     print(*map(get_cube, filter(select_num, numbers)), sep='\n')
-    # print(filter(select_num, numbers), sep='\n')
+
+
+def reduce(operation, items, initial_value):
+    acc = initial_value
+    for item in items:
+        acc = operation(acc, item)
+    return acc
+
+
+def squares(num):
+    return num ** 2
+
+
+def summa(x, y):
+    return x + y ** 2
+
+
+def output_summa():
+    """
+    Программа для вычисления и вывода суммы квадратов элементов списка numbers.
+    """
+    numbers = [97, 42, 9, 32, 3, 45, 31, 77, -1, 11, -2, 75, 5, 51, 34, 28, 46, 1, -8, 84, 16, 51, 90, 56,
+               65, 90, 23, 35, 11, -10, 70, 90, 90, 12, 96, 58, -8, -4, 91, 76, 94, 60, 72, 43, 4, -6, -5,
+               51, 58, 60, 30, 38, 67, 62, 36, 72, 34, 82, 62, -1, 60, 82, 87, 81, -7, 57, 26, 36, 17, 43,
+               80, 40, 75, 94, 91, 64, 38, 72, 29, 84, 38, 35, 7, 54, 31, 95, 78, 27, 82, 1, 64, 94, 31, 29,
+               -8, 98, 24, 61, 7, 73]
+
+    print(sum(map(squares, numbers)))
+    print(reduce(summa, numbers, 0))
+
+
+def get_two_digit_numbers(num):
+    return len(str(abs(num))) == 2 and num % 7 == 0
+
+
+def output_summa_square():
+    """
+    Программа для вычисления и вывода суммы квадратов двузначных чисел, которые делятся на 7 без остатка.
+    """
+    numbers = [77, 293, 28, 242, 213, 285, 71, 286, 144, 276, 61, 298, 280, 214, 156, 227, 228, 51, -4, 202, 58, 99,
+               270, 219, 94, 253, 53, 235, 9, 158, 49, 183, 166, 205, 183, 266, 180, 6, 279, 200, 208, 231, 178, 201,
+               260, -35, 152, 115, 79, 284, 181, 92, 286, 98, 271, 259, 258, 196, -8, 43, 2, 128, 143, 43, 297, 229, 60,
+               254, -9, 5, 187, 220, -8, 111, 285, 5, 263, 187, 192, -9, 268, -9, 23, 71, 135, 7, -161, 65, 135, 29,
+               148, 242, 33, 35, 211, 5, 161, 46, 159, 23, 169, 23, 172, 184, -7, 228, 129, 274, 73, 197, 272, 54, 278,
+               26, 280, 13, 171, 2, 79, -2, 183, 10, 236, 276, 4, 29, -10, 41, 269, 94, 279, 129, 39, 92, -63, 263, 219,
+               57, 18, 236, 291, 234, 10, 250, 0, 64, 172, 216, 30, 15, 229, 205, 123, -105]
+
+    print(sum(map(squares, filter(get_two_digit_numbers, numbers))))
+
+
+def func_apply(func, items):
+    """
+     Функция func_apply(), принимающую на вход функцию и список значений и возвращающую список,
+     в котором каждое значение будет результатом применения переданной функции к переданному списку.
+    """
+    return list(map(func, items))
+
+
+def add3(x):
+    return x + 3
+
 
 def main():
-    selects_three_digit_numbers()
+    print(func_apply(add3, [1, 2, 3, 4, 5, 6]))
+    # output_summa_square()
+    # output_summa()
+    # selects_three_digit_numbers()
     # rounds_2()
     # interesting_sorting_2()
     # interesting_sorting_1()
