35a3f4e2909393acc8e6ef5b26d8c1e11465c43e
GenyaElamkov <GenyaElamkov@gmail.com>
2023-01-03 15:14:12 +0300

    [upd] модуль functions-> модуль 7 закрыт

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 from math import sqrt, sin
+import operator
 
 
 def matrix(n=1, m=None, value=0):
@@ -345,8 +346,64 @@ def add3(x):
     return x + 3
 
 
+"""
+Требовалось написать программу, которая:
+
+преобразует список floats в список чисел, возведенных в квадрат и округленных с 
+точностью до одного десятичного знака;
+фильтрует список words  и оставляет только палиндромы длиной более 4 символов;
+находит произведение чисел из списка numbers.
+Программист торопился и написал программу неправильно. Доработайте его программу.
+"""
+floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59, 34.23, 12.12, 4.67, 2.45, 9.32]
+words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
+numbers = [4, 6, 9, 23, 5]
+
+# Исправьте этот код
+map_result = list(map(lambda num: round(num ** 2, 1), floats))
+filter_result = list(filter(lambda name: name == name[::-1] and len(name) > 4, words))
+reduce_result = reduce(lambda num1, num2: num1 * num2, numbers, 1)
+
+"""
+Напишите программу, которая с помощью встроенных функций filter(), map(), sorted() и reduce() 
+выводит в алфавитном порядке список primary городов с населением более 10000000 человек, в формате:
+
+Cities: Beijing, Buenos Aires, ...
+"""
+data = [['Tokyo', 35676000, 'primary'],
+        ['New York', 19354922, 'nan'],
+        ['Mexico City', 19028000, 'primary'],
+        ['Mumbai', 18978000, 'admin'],
+        ['Sao Paulo', 18845000, 'admin'],
+        ['Delhi', 15926000, 'admin'],
+        ['Shanghai', 14987000, 'admin'],
+        ['Kolkata', 14787000, 'admin'],
+        ['Los Angeles', 12815475, 'nan'],
+        ['Dhaka', 12797394, 'primary'],
+        ['Buenos Aires', 12795000, 'primary'],
+        ['Karachi', 12130000, 'admin'],
+        ['Cairo', 11893000, 'primary'],
+        ['Rio de Janeiro', 11748000, 'admin'],
+        ['Osaka', 11294000, 'admin'],
+        ['Beijing', 11106000, 'primary'],
+        ['Manila', 11100000, 'primary'],
+        ['Moscow', 10452000, 'primary'],
+        ['Istanbul', 10061000, 'admin'],
+        ['Paris', 9904000, 'primary']]
+
+filter_primary = list(filter(lambda word: 'primary' in word, data))
+filter_million = list(map(lambda n: n[0], filter(lambda n: n[1] > 10_000_000, filter_primary)))
+sort_filter_million = sorted(filter_million)
+print_res = reduce(lambda x, y: x + ' ' + y + ',', sort_filter_million, 'Cities:')[:-1]
+# s = list(filter(lambda x:x[1] > 10_000_000 and x[2] == 'primary', data))
+
+
 def main():
-    print(func_apply(add3, [1, 2, 3, 4, 5, 6]))
+    print(print_res)
+    # print(map_result)
+    # print(filter_result)
+    # print(reduce_result)
+    # print(func_apply(add3, [1, 2, 3, 4, 5, 6]))
     # output_summa_square()
     # output_summa()
     # selects_three_digit_numbers()
